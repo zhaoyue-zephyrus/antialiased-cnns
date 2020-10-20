ba43f46ec66e5a69900df77e127ae5bc26e4b2ad
zhaoyue-zephyrus <thuzhaoyue@gmail.com>
2020-10-21 07:30:57 +0800

    add blurpool3d

--- antialiased_cnns/blurpool.py
+++ antialiased_cnns/blurpool.py
@@ -114,3 +114,78 @@ def get_pad_layer_1d(pad_type):
     else:
         print('Pad type [%s] not recognized' % pad_type)
     return PadLayer
+
+class BlurPool3d(nn.Module):
+    def __init__(self, channels, pad_type='reflect', filt_size_t=1, filt_size=4,
+                 stride_t=1, stride=2, pad_off_t=0, pad_off=0):
+        super(BlurPool3d, self).__init__()
+        self.filt_size = filt_size
+        self.pad_off = [pad_off_t, pad_off, pad_off]
+        self.pad_sizes = [int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2)),
+                          int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2)),
+                          int(1.*(filt_size_t-1)/2),int(1.*(filt_size_t-1)/2)]
+        self.pad_sizes = [pad_size+pad_off
+                            for pad_size, pad_off in zip(*(self.pad_sizes, self.pad_off))]
+        self.stride_t = stride_t
+        self.stride = stride
+        self.off = int((self.stride-1)/2.)
+        self.channels = channels
+
+        if(self.filt_size==1):
+            a = np.array([1.,])
+        elif(self.filt_size==2):
+            a = np.array([1., 1.])
+        elif(self.filt_size==3):
+            a = np.array([1., 2., 1.])
+        elif(self.filt_size==4):    
+            a = np.array([1., 3., 3., 1.])
+        elif(self.filt_size==5):    
+            a = np.array([1., 4., 6., 4., 1.])
+        elif(self.filt_size==6):    
+            a = np.array([1., 5., 10., 10., 5., 1.])
+        elif(self.filt_size==7):    
+            a = np.array([1., 6., 15., 20., 15., 6., 1.])
+
+        if(self.filt_size_t==1):
+            b = np.array([1.,])
+        elif(self.filt_size_t==2):
+            b = np.array([1., 1.])
+        elif(self.filt_size_t==3):
+            b = np.array([1., 2., 1.])
+        elif(self.filt_size_t==4):    
+            b = np.array([1., 3., 3., 1.])
+        elif(self.filt_size_t==5):    
+            b = np.array([1., 4., 6., 4., 1.])
+        elif(self.filt_size_t==6):    
+            b = np.array([1., 5., 10., 10., 5., 1.])
+        elif(self.filt_size_t==7):    
+            b = np.array([1., 6., 15., 20., 15., 6., 1.])
+
+        filt = torch.Tensor(b[:,None,None]*a[None,:,None]*a[None,None,:])
+        filt = filt/torch.sum(filt)
+        self.register_buffer('filt', filt[None,None,:,:,:].repeat((self.channels,1,1,1,1)))
+
+        self.pad = get_pad_layer_3d(pad_type)(self.pad_sizes)
+
+    def forward(self, inp):
+        if(self.filt_size == 1 and self.filt_size_t == 1):
+            if(self.pad_off == 0 and self.pad_off_t == 0):
+                return inp[:,:,:,::self.stride,::self.stride]    
+            else:
+                return self.pad(inp)[:,:,:,::self.stride,::self.stride]
+        else:
+            return F.conv3d(self.pad(inp), self.filt, stride=(self.stride_t, self.stride, self.stride), groups=inp.shape[1])
+
+def get_pad_layer_3d(pad_type):
+    if(pad_type in ['refl','reflect']):
+        from reflection_pad3d import ReflectionPad3d
+        # TODO: add to pytorch
+        PadLayer = ReflectionPad3d
+    elif(pad_type in ['repl','replicate']):
+        PadLayer = nn.ReplicationPad3d
+    elif(pad_type=='zero'):
+        # TODO: implemented this
+        PadLayer = nn.ZeroPad3d
+    else:
+        print('Pad type [%s] not recognized'%pad_type)
+    return PadLayer
